8d891ab98717b058fe7ee8c7e447695688b0ad6b
ironholds <ironholds@gmail.com>
2015-04-17 18:30:54 -0400

    many things

--- R/log_file.R
+++ R/log_file.R
@@ -1,36 +1,69 @@
 # Declare the .addCondHands function to inform R CMD check about it.
 globalVariables(".addCondHands")
 
-#' Activate a log file
+#' @title Activate a log file
 #'
-#' @param file_name character: path to the log file.
+#' @description \code{log_file} creates an active instance
+#' of a log file that loggr can pass errors, warnings and messages
+#' on to. If this file already exists, it will be appended
+#' to unless \code{overwrite} is set to TRUE.
+#'
+#' @param file_name the path to the log file.
+#'
 #' @param ... list of quoted or unquoted events to log. In none are
 #'   provided all log events will be captured.
+#'
 #' @param .warning logical: capture regular warnings (\code{simpleWarning})?
+#'
 #' @param .error logical: capture regular errors (\code{simpleError})?
+#'
 #' @param .message logical: capture regular messages (\code{simpleMessage})?
+#'
 #' @param .formatter logical: the formatting function to use to convert
 #'   a log event to its character representation.
+#'
+#' @param overwrite whether or not to overwrite the file at \code{file_name}
+#' if it already exists. Set to FALSE by default.
+#'
 #' @return NULL invisibly
+#'
+#' @examples
+#'
+#' #Create a "default" log file instance
+#' log_file()
+#'
+#' #Create a log file instance that only checks errors
+#' log_file("errors_only_thanks.log", .warning = FALSE, .message = FALSE)
+#'
 #' @export
 log_file <- function(file_name, ...,
                      .warning   = TRUE, .error = TRUE, .message = TRUE,
-                     .formatter = format_log_entry)
-{
-  if (file_name == "")
+                     .formatter = format_log_entry, overwrite = FALSE){
+
+  if (!is.vector(file_name, "character") || file_name == ""){
     stop("Please provide a valid file name.", call. = FALSE)
+  }
+
 
   # Make sure logging hooks have been setup.
   use_logging()
 
   if (!file_name %in% c("stdout", "console")) {
-    # Setup a new log file, or continue with existing.
-    action <- ifelse(!file.exists(file_name), "created", "continued with")
-    if (action == "created") {
+
+    #If the file doesn't exist, create it. If the file
+    #does exist, and overwrite is TRUE, delete and replace.
+    #If the file does exist and overwrite is FALSE, just append.
+    if(!file.exists(file_name)){
+      action <- "created"
       file.create(file_name)
+    } else if(file.exists(file_name) & overwrite){
+      action <- "created"
+      file.remove(file_name)
+      file.create(file_name)
+    } else {
+      action <- "continued with"
     }
-    init_event <- log_event("INFO",
-                            sprintf("R session %s this log file.\n", action))
+    init_event <- log_event("INFO", sprintf("R session %s this log file.\n", action))
     cat(.formatter(init_event), "\n", file = file_name, append = TRUE)
   }
 
